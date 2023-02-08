cb0ce14693d6bb06dffd2fb6118a55195a7d0b14
KiSEiGitHub <tommusique974@gmail.com>
2023-02-08 22:18:38 +0100

    :fix: id session

--- index.php
+++ index.php
@@ -11,10 +11,18 @@
             <i class="fa-solid fa-house text-xl"></i>
             <a href="./?page=home" class='item-nav'>Home</a>
          </div>
-         <div class='flex gap-4 items-center text-accent'>
-            <i class="fa-regular fa-user text-xl"></i>
-            <a href="./?page=profile" class='item-nav'>Profile</a>
-         </div>
+
+         <!-- SI le user n'est pas connecté alors il n'a pas accès à son profil -->
+         <!-- s'affiche uniquement si le user est connecté  -->
+         <?php if (isset($_SESSION['id'])) : ?>
+            <div class='flex gap-4 items-center text-accent'>
+               <i class="fa-regular fa-user text-xl"></i>
+               <a href="./?page=profile" class='item-nav'>Profile</a>
+            </div>
+         <?php endif; ?>
+         <!-- s'affiche uniquement si le user est connecté  -->
+         <!-- SI le user n'est pas connecté alors il n'a pas accès à son profil -->
+
          <div class='flex gap-4 items-center text-accent'>
             <i class="fa-solid fa-gamepad text-xl"></i>
             <a href="./?page=games" class='item-nav'>Games</a>
@@ -24,23 +32,23 @@
       <div class='flex flex-col gap-4'>
          <!-- Si on est pas connecté  -->
          <?php if (!isset($_SESSION['id'])) : ?>
-         <div class='flex gap-4 items-center text-accent'>
-            <i class="fa-solid fa-user-plus"></i>
-            <a href="./?page=inscription" class='item-nav'>Inscription</a>
-         </div>
-         <div class='flex gap-4 items-center text-accent'>
-            <i class="fa-solid fa-right-to-bracket"></i>
-            <a href="./?page=connexion" class='item-nav'>Connexion</a>
-         </div>
+            <div class='flex gap-4 items-center text-accent'>
+               <i class="fa-solid fa-user-plus"></i>
+               <a href="./?page=inscription" class='item-nav'>Inscription</a>
+            </div>
+            <div class='flex gap-4 items-center text-accent'>
+               <i class="fa-solid fa-right-to-bracket"></i>
+               <a href="./?page=connexion" class='item-nav'>Connexion</a>
+            </div>
          <?php endif; ?>
          <!-- Si on est pas connecté  -->
 
          <!-- Si on est connecté -->
          <?php if (isset($_SESSION['id'])) : ?>
-         <div class='flex gap-4 items-center text-accent'>
-            <i class="fa-solid fa-right-from-bracket"></i>
-            <a href="./?page=deconnexion" class='item-nav'>Déconnexion</a>
-         </div>
+            <div class='flex gap-4 items-center text-accent'>
+               <i class="fa-solid fa-right-from-bracket"></i>
+               <a href="./?page=deconnexion" class='item-nav'>Déconnexion</a>
+            </div>
          <?php endif; ?>
          <!-- Si on est connecté -->
       </div>
