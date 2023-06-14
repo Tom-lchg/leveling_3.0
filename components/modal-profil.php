ffdfac61b3a7bb73765dfbf397dfd44710ae4031
SalimCodiing <alarconsalim95@gmail.com>
2023-06-14 22:27:20 +0200

    maj modal

--- components/modal-profil.php
+++ components/modal-profil.php
@@ -133,9 +133,9 @@ if (isset($_SESSION['id'])) {
 
       <form action="../handler_formulaire/handler.php" method="POST" class='mt-4 w-full' enctype="multipart/form-data">
          <div class='flex flex-col gap-4 w-full '>
-            <input type="hidden" name="idgroupe" value="<?= isset($_GET['groupe']) ? $_GET['groupe'] : '' ?>">
-            <input type="hidden" name="idauteur" value="<?= isset($_SESSION['id']) ? $_SESSION['id'] : '' ?>">
-            <input type="hidden" name="privacy" value="<?= isset($_GET['privacy']) ? $_GET['privacy'] : '' ?>">
+            <input type="hidden" name="idgroupe" value="<?=$_GET['groupe']?>">
+            <input type="hidden" name="idauteur" value="<?=$_SESSION['id']?>">
+            <input type="hidden" name="privacy" value="<?=$_GET['privacy']?>">
             <input required="required" maxlength="100" class='input input-bordered w-xs placeholder-[#CCCCCF] placeholder:text-sm' type="text" placeholder="Titre du sujet" name='titreSujet'>
             <textarea class='textarea textarea-accent text-md textarea-xl' placeholder='Corp du sujet' name='descSujet'></textarea>
             <button type='submit' name='btn-add-topic' class='btn btn-accent'>Créer votre sujet</button>
@@ -156,10 +156,10 @@ if (isset($_SESSION['id'])) {
 
       <form action="../handler_formulaire/handler.php" method="POST" class='mt-4 w-full' enctype="multipart/form-data">
          <div class='flex flex-col gap-4 w-full '>
-            <input type="hidden" name="idsujet" value="<?= isset($_GET['sujet']) ? $_GET['sujet'] : '' ?>">
-            <input type="hidden" name="idauteur" value="<?= isset($_SESSION['id']) ? $_SESSION['id'] : '' ?>">
-            <input type="hidden" name="idgroupe" value="<?= isset($_GET['groupe']) ? $_GET['groupe'] : '' ?>">
-            <input type="hidden" name="privacy" value="<?= isset($_GET['privacy']) ? $_GET['privacy'] : '' ?>">
+            <input type="hidden" name="idsujet" value="<?=$_GET['sujet']?>">
+            <input type="hidden" name="idauteur" value="<?=$_SESSION['id']?>">
+            <input type="hidden" name="idgroupe" value="<?=$_GET['groupe']?>">
+            <input type="hidden" name="privacy" value="<?$_GET['privacy']?>">
             <textarea class='textarea textarea-accent text-md textarea-xl' placeholder='Corp de la réponse' name='descAnswers'></textarea>
             <button type='submit' name='btn-answer-topic' class='btn btn-accent'>Publier votre réponse</button>
          </div>
@@ -180,27 +180,27 @@ if (isset($_SESSION['id'])) {
       <?php 
       $oneGroupePublic = null; 
       $oneGroupePrive = null;
-      if(isset($_GET['privacy']) ? $_GET['privacy'] : '' == "publique"){
-         $oneGroupePublic = $controler->groupe->groupeModel->getOneGroupPublic(isset($_GET['groupe']) ? $_GET['groupe']: '');
+      if($_GET['privacy'] == "publique"){
+         $oneGroupePublic = $controler->groupe->groupeModel->getOneGroupPublic($_GET["groupe"]);
       }else{
-         $oneGroupePrive = $controler->groupe->groupeModel->getOneGroupPrive(isset($_GET['groupe']) ? $_GET['groupe']: '');
+         $oneGroupePrive = $controler->groupe->groupeModel->getOneGroupPrive($_GET["groupe"]);
       }
       ?>
 
       <form action="../handler_formulaire/handler.php" method="POST" class='mt-4 w-full' enctype="multipart/form-data">
          <div>
 
-         <input type="hidden" name="idgroupe" value="<?= isset($_GET['groupe']) ? $_GET['groupe'] : '' ?>">
-         <input type="hidden" name="privacy" value="<?= isset($_GET['privacy']) ? $_GET['privacy'] : '' ?>">
+         <input type="hidden" name="idgroupe" value="<?= $_GET['groupe'] ?>">
+         <input type="hidden" name="privacy" value="<?= $_GET['privacy'] ?>">
 
          <div class="mt-4 w-full">
             <label><p class="text-sm">Nom</p></label>
-            <input required="required" maxlength="20" class='input input-bordered w-120 placeholder-[#CCCCCF] placeholder:text-sm text-neutral' type="text" value="<?= isset($oneGroupePrive) && isset($oneGroupePrive['groupeName']) ? $oneGroupePrive['groupeName'] : (isset($oneGroupePublic['groupeName']) ? $oneGroupePublic['groupeName'] : '') ?>" name='update-title'>
+            <input required="required" maxlength="20" class='input input-bordered w-120 placeholder-[#CCCCCF] placeholder:text-sm text-neutral' type="text" value="<?= $oneGroupePrive==null ? $oneGroupePublic['groupeName'] : $oneGroupePrive['groupeName'] ?>" name='update-title'>
          </div>
 
          <div class="mt-4 w-full">
             <label><p class="text-sm">Description</p></label>
-            <textarea required="required" maxlength="100" class='textarea textarea-bordered w-full placeholder-[#CCCCCF] placeholder:text-sm text-neutral' type="text" name='update-desc'><?= isset($oneGroupePrive) && isset($oneGroupePrive['groupeDescription']) ? $oneGroupePrive['groupeDescription'] : (isset($oneGroupePublic['groupeDescription']) ? $oneGroupePublic['groupeDescription'] : '') ?></textarea>
+            <textarea required="required" maxlength="100" class='textarea textarea-bordered w-full placeholder-[#CCCCCF] placeholder:text-sm text-neutral' type="text" name='update-desc'><?= $oneGroupePrive===null ? $oneGroupePublic['groupeDescription'] : $oneGroupePrive['groupeDescription'] ?></textarea>
          </div>
 
          <div class="mt-4 w-full">
@@ -216,12 +216,11 @@ if (isset($_SESSION['id'])) {
       <form action="../handler_formulaire/handler.php" method="POST" class='mt-4 w-full' enctype="multipart/form-data">
       <div>
 
-      <input type="hidden" name="idgroupe" value="<?= isset($_GET['groupe']) ? $_GET['groupe'] : '' ?>">
-      <input type="hidden" name="privacy" value="<?= isset($_GET['privacy']) ? $_GET['privacy'] : '' ?>">
+      <input type="hidden" name="idgroupe" value="<?= $_GET['groupe'] ?>">
+      <input type="hidden" name="privacy" value="<?= $_GET['privacy'] ?>">
 
       <div class='mt-4 w-full flex items-center'>
-      <img src="data:<?= isset($oneGroupePrive) && isset($oneGroupePrive['groupeTypeImg']) ? $oneGroupePrive['groupeTypeImg'] : (isset($oneGroupePublic['groupeTypeImg']) ? $oneGroupePublic['groupeTypeImg'] : '') ?>;base64,<?= isset($oneGroupePrive) && isset($oneGroupePrive['groupeImg']) ? base64_encode($oneGroupePrive['groupeImg']) : (isset($oneGroupePublic['groupeImg']) ? base64_encode($oneGroupePublic['groupeImg']) : '') ?>" class='w-20 h-20 rounded-full shadow-lg' alt="">
-
+            <img src="data:<?= $oneGroupePrive===null ? $oneGroupePublic['groupeTypeImg'] : $oneGroupePrive['groupeTypeImg'] ?>;base64,<?= $oneGroupePrive===null ? base64_encode($oneGroupePublic['groupeImg']) :  base64_encode($oneGroupePrive['groupeImg'])?>" class='w-20 h-20 rounded-full shadow-lg' alt="">
             <input type="file" class="file-input file-input-bordered file-input-md w-full max-w-xs ml-4" name="update-group-profil" />
             <p class="ml-4 font-bold text-neutral text-sm"><i class="fa-sharp fa-solid fa-circle-exclamation"></i> JPG UNIQUEMENT</p>
       </div>
@@ -237,16 +236,12 @@ if (isset($_SESSION['id'])) {
 
       <form action="../handler_formulaire/handler.php" method="POST" class='mt-4 w-full' enctype="multipart/form-data">
       <div>
-      
-         <input type="hidden" name="idgroupe" value="<?= isset($_GET['groupe']) ? $_GET['groupe'] : '' ?>">
-         <input type="hidden" name="privacy" value="<?= isset($_GET['privacy']) ? $_GET['privacy'] : '' ?>">
+
+      <input type="hidden" name="idgroupe" value="<?= $_GET['groupe'] ?>">
+      <input type="hidden" name="privacy" value="<?= $_GET['privacy'] ?>">
 
       <div class='mt-4 w-full flex items-center'>
-      <?php if ($oneGroupePrive === null): ?>
-         <img src="data:<?= isset($oneGroupePublic['groupeTypeBanner']) ? $oneGroupePublic['groupeTypeBanner'] : '' ?>;base64,<?= isset($oneGroupePublic['groupeBanner']) ? base64_encode($oneGroupePublic['groupeBanner']) : '' ?>" alt="banner" class='h-20 w-40 rounded-lg'>
-      <?php else: ?>
-         <img src="data:<?= isset($oneGroupePrive['groupeTypeBanner']) ? $oneGroupePrive['groupeTypeBanner'] : '' ?>;base64,<?= isset($oneGroupePrive['groupeBanner']) ? base64_encode($oneGroupePrive['groupeBanner']) : '' ?>" alt="banner" class='h-20 w-40 rounded-lg'>
-      <?php endif; ?>
+            <img src="data:<?= $oneGroupePrive===null ? $oneGroupePublic['groupeTypeBanner'] : $oneGroupePrive['groupeTypeBanner'] ?>;base64,<?= $oneGroupePrive===null ? base64_encode($oneGroupePublic['groupeBanner']) :  base64_encode($oneGroupePrive['groupeBanner'])?>" alt="banner" class='h-20 w-40 rounded-lg'>
             <input type="file" class="file-input file-input-bordered file-input-md w-full max-w-xs ml-4" name="update-group-banner" />
             <p class="ml-4 font-bold text-neutral text-sm"><i class="fa-sharp fa-solid fa-circle-exclamation"></i> JPG UNIQUEMENT</p>
       </div>
@@ -262,8 +257,8 @@ if (isset($_SESSION['id'])) {
 
       <form action="../handler_formulaire/handler.php" method="POST" class='mt-4 w-full' enctype="multipart/form-data">
 
-         <input type="hidden" name="idgroupe" value="<?= isset($_GET['groupe']) ? $_GET['groupe'] : '' ?>">
-         <input type="hidden" name="privacy" value="<?= isset($_GET['privacy']) ? $_GET['privacy'] : '' ?>">
+         <input type="hidden" name="idgroupe" value="<?= $_GET['groupe'] ?>">
+         <input type="hidden" name="privacy" value="<?= $_GET['privacy'] ?>">
 
          <div class='mt-4 w-full'>
             <button type='submit' name='btn-del-group' class="btn btn-error w-full">Supprimer ce groupe</button>
@@ -282,7 +277,7 @@ if (isset($_SESSION['id'])) {
    <div class="modal-box relative bg-secondary max-w-3xl">
       <label for="modal-add-user-groupe" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
       <h3 class="text-lg font-bold">AJOUTER UNE PERSONNE :</h3>
-      <?php $allUserNotInGroupe = $controler->groupe->groupeModel->getUserNotOnGroupe(isset($_GET['groupe']) ? $_GET['groupe'] : '');?>
+      <?php $allUserNotInGroupe = $controler->groupe->groupeModel->getUserNotOnGroupe($_GET['groupe']);?>
       <center>
       <form action="../handler_formulaire/handler.php" method="POST" class='mt-4 w-full' enctype="multipart/form-data">
          <select class="select w-full max-w-xs" name="selectedUser">
@@ -291,8 +286,8 @@ if (isset($_SESSION['id'])) {
             <option value="<?= $oneUser['idUser'] ?>"><?= $oneUser['userPseudo'] ?></option>
             <?php endforeach; ?>
          </select><br><br>
-         <input type="hidden" name="idgroupe" value="<?= isset($_GET['groupe']) ? $_GET['groupe'] : '' ?>">
-         <input type="hidden" name="privacy" value="<?= isset($_GET['privacy']) ? $_GET['privacy'] : '' ?>">">
+         <input type="hidden" name="idgroupe" value="<?= $_GET['groupe'] ?>">
+         <input type="hidden" name="privacy" value="<?= $_GET['privacy'] ?>">">
          <button type='submit' name='btn-add-user-groupe' class='btn btn-accent'>Ajouter cette personne</button>
    </div>
 
--- mvc/model/Games.php
+++ mvc/model/Games.php
@@ -37,4 +37,3 @@ class Games
    {
       return $this->model->findByIdCs($id, $target);
    }
-}
